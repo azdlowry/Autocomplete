a6c84c569062eac6f99c06961dde1b4fc1760e54
Andy Lowry <andy@andylowry.me.uk>
2015-11-10 21:40:13 +0000

    Initial commit

---
+++ Autocomplete.API.Tests/APITests.cs
@@ -0,0 +1,32 @@
+﻿using Nancy;
+using Nancy.Testing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Autocomplete.API.Tests
+{
+    public class APITests
+    {
+        public void reference_to_pull_in_types()
+        {
+            new AutocompleteModule();
+        }
+
+        [Fact]
+        public void Should_return_status_ok_for_root()
+        {
+            var bootstrapper = new DefaultNancyBootstrapper();
+            var browser = new Browser(bootstrapper);
+            
+            var result = browser.Get("/", with => {
+                with.HttpRequest();
+            });
+            
+            Assert.Equal(HttpStatusCode.OK, result.StatusCode);
+        }
+    }
+}
